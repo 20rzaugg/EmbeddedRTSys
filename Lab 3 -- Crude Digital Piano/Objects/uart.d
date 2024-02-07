28d18a617766ba6cce56b690f7f30c2e19797033
kmskillings <kmskillings@gmail.com>
2024-02-07 13:32:37 -0700

    Lab 3 now works identically to Lab 2.

--- Lab 3 -- Crude Digital Piano/Objects/uart.d
+++ Lab 3 -- Crude Digital Piano/Objects/uart.d
@@ -7,4 +7,5 @@
   C:\Users\18012\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\.\m-profile\cmsis_armclang_m.h \
   C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
   C:\Users\18012\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\m-profile\armv7m_mpu.h \
-  C:\Users\18012\AppData\Local\Arm\Packs\Keil\STM32L4xx_DFP\2.6.1\Drivers\CMSIS\Device\ST\STM32L4xx\Include\system_stm32l4xx.h
+  C:\Users\18012\AppData\Local\Arm\Packs\Keil\STM32L4xx_DFP\2.6.1\Drivers\CMSIS\Device\ST\STM32L4xx\Include\system_stm32l4xx.h \
+  gpio.h
--- Lab 3 -- Crude Digital Piano/tim.h
+++ Lab 3 -- Crude Digital Piano/tim.h
@@ -4,6 +4,6 @@
 #define UPCOUNT 0
 #define DOWNCOUNT 1
 
-void useHSI();
+void useHSI(void);
 
 void enableTimer(TIM_TypeDef *TIMx, uint32_t prescaler, uint32_t aar, uint8_t direction, uint8_t generateInterrupt);
